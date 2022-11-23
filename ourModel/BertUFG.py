3a9edf5ffaeaaaf0ba0b5756aa748e884cb98324
qinZhongzheng <91660829+qinZhongzheng@users.noreply.github.com>
2022-11-23 13:39:30 +0800

    Update BertUFG.py

--- ourModel/BertUFG.py
+++ ourModel/BertUFG.py
@@ -256,8 +256,8 @@ model = Net(nhid, nb_class, r, Lev, nb_node,m,shrinkage=None,
                     threshold=1e-3, dropout_prob=args.dropout).to(device)
 
 # if th.cuda.device_count() > 1:
-model = nn.DataParallel(model)
-model = model.module
+# model = nn.DataParallel(model)
+# model = model.module
 
 input_ids, attention_mask = encode_input(text, model.tokenizer)
 input_ids = th.cat([input_ids[:-nb_test], th.zeros((nb_word, max_length), dtype=th.long), input_ids[-nb_test:]])
